6096645f2559dd9d08641c0bae52f0c21c97f8e9
sammwaura <meshsami27@gmail.com>
2018-03-06 08:05:00 +0300

    modify user interface to the javascript file

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,13 @@ $(document).ready(function() {
   $("form#form").submit(function(event) {
     event.preventDefault();
     var name = $("input#player1").val();
-    $("#titles").append(name);
+    $("#title").append(name);
     $("form#form").hide();
   });
+  $("form#form2").submit(function(event) {
+   event.preventDefault();
+   var name = $("input#player2").val();
+   $("#title").append(name);
+   $("form#form2").hide();
+ });
+ 
